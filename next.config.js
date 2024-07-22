f461d70f982b20c292a2d32657f45fb44285e304
Akshit Gupta <akshu.24gupta@gmail.com>
2024-07-22 17:01:34 +0530

    some minor error

--- next.config.js
+++ next.config.js
@@ -1,31 +1,28 @@
 // @ts-check
- 
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    /* config options here */
-    webpack: (config) => {
-      config.externals.push({
-        "utf-8-validate": "commonjs utf-8-validate",
-        bufferutil: "commonjs bufferutil",
-        canvas: "commonjs canvas",
-      });
-      // config.infrastructureLogging = { debug: /PackFileCache/ };
-      return config;
-    },
+  /* config options here */
+  webpack: (config) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil",
+      canvas: "commonjs canvas",
+    });
+    // config.infrastructureLogging = { debug: /PackFileCache/ };
+    return config;
+  },
 
 
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'utfs.io'
-        }
-      ]
-    },
-  typescript : {
-    ignoreBuildErrors: true
-  }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io'
+      }
+    ]
+  },
 
-  }
-   
-  module.exports = nextConfig
+}
+
+module.exports = nextConfig
